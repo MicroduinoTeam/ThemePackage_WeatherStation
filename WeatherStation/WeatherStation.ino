60fe7c9306e24a1e71e30479dc0bdf2378b3e9c8
wasdylb <853387039@qq.com>
2018-01-03 12:47:53 +0800

    修改初始状态下无检测到Light-D1传感器，自动切换成模拟光照传感器

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -9,19 +9,25 @@ SoftwareSerial mySerial(4, -1); // RX, TX
 Dust pmSensor(&mySerial);     //PM2.5使用软串口（4，-1）
 
 #define INTERVAL_LCD    500   //OLED及传感器刷新时间间隔   
- 
-uint32_t long lcd_time; 
+
+#define LIGHTD1PIN A0
+
+uint32_t long lcd_time;
 float sensorTemp;       //温度值
 float sensorHumi;       //湿度值
 float sensorPM25;       //pm2.5浓度
 float sensorLight;      //光照强度
 
+boolean LightSta;
+
 void setup(void) {
-  
+
   Serial.begin(115200);     //初始化波特率
   termo.begin();            //初始化温湿度
-  tsl.begin();              //初始化光照
-  tsl.enableAutoGain(true); //设置光照自动增益
+  LightSta = tsl.begin(); //初始化光照
+  if (LightSta) {
+    tsl.enableAutoGain(true); //设置光照自动增益
+  }
 }
 
 void loop(void) {
@@ -30,8 +36,12 @@ void loop(void) {
 
     sensorTemp = termo.getTemperature();  //获取温度
     sensorHumi = termo.getHumidity();    //获取湿度
-    sensorLight = tsl.getLuminosity();   //获取光照
-
+    if (LightSta) {
+      sensorLight = tsl.getLuminosity();   //获取光照
+    }
+    else {
+      sensorLight = analogRead(LIGHTD1PIN);
+    }
     if (pmSensor.available())
       sensorPM25 = pmSensor.getPM25();   //获取PM2.5
 
